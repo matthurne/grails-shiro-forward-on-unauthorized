8787b23b324033690837398914e063c47ed2e0d4
Matt Hurne <matt@thehurnes.com>
2013-07-26 18:45:56 -0400

    During BootStrap, create test user with no permissions

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,14 @@
+import org.apache.shiro.crypto.hash.Sha256Hash
+
 class BootStrap {
 
     def init = { servletContext ->
+        def user = new ShiroUser(username: "user123", passwordHash: new Sha256Hash("password").toHex())
+        user.save()
     }
+
     def destroy = {
     }
+
 }
+
